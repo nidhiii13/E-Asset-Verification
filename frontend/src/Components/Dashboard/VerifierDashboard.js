5ea64f1db5ba0ca592aa4b26403139218c6a3a3e
monika <monikas.is19@rvce.edu.in>
2021-12-21 20:37:47 +0530

    verifierdashboard version2 added

--- frontend/src/Components/Dashboard/VerifierDashboard.js
+++ frontend/src/Components/Dashboard/VerifierDashboard.js
@@ -1,27 +1,31 @@
 import React from 'react'
-import "./Dashboard2.css";
+import './Dashboard2.css'
 import $ from 'jquery';
 import { Link } from 'react-router-dom'
 const VerifierDashboard = () => {
 
     return (
         <>
-        <div className='verifierdashboard_container'>
-            <div className="sidebar">
-                <dropdown>
-                    <ul className='sidebar_nav'>
-                        <li className='list'>Scan Barcode</li>
-                        <input id="drop4" type="checkbox" />
-                        <label for="drop4" className="animate">Asset Verfication</label>
-                        <ul>
-                            <li className='list'>Found Report</li>
-                            <li className='list'>Not Found Report</li>
+            <nav className='nav_container'>
+                <ul>
+                    <li><a href="#" title="Nach Hause" className='list_value1'>Scan Barcode</a></li>
+                    <li className="sub">
+                        <input type="checkbox" />
+                        <a href="#" className='list_value'>Asset Verication</a>
+                        <ul className="submenu">
+                            <li className="sub">
+                                <input type="checkbox" />
+                                <a href="#" className='list_value'>Found Report</a>
+                            </li>
+                            <li className="sub">
+                                <input type="checkbox" />
+                                <a href="#" className='list_value' >Not found report</a>
+                            </li>
                         </ul>
-                        <li className="drop">Help</li>
-                    </ul>
-                </dropdown>
-            </div>
-            </div>
+                    </li>
+                    <li><a href="#" className='list_value3'>Logout</a></li>
+                </ul>
+            </nav>
         </>
     )
 }
